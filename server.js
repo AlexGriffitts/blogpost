ed24d7fbccac61a29a5b60be299542c58f967a84
AGriffitts <0alexgriffitts0@gmail.com>
2023-02-11 23:57:16 -0600

    trying to make heroku work

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ const Sequelizestore = require('connect-session-sequelize')(session.Store);
 const routes = require('./controllers');
 const http = require('http');
 const app = express();
-const PORT = process.env.PORT || 3000;
-const path = require('path');
-const hostname = `localhost`;
-const port = 3000;
+// const PORT = process.env.PORT || 3000;
+// const path = require('path');
+// const hostname = `localhost`;
+// const port = 3000;
 
-const server = http.createServer((req, res) =>{
-  res.statusCode = 200;
-  res.setHeader('Content-type', 'text/plain');
-  res.end('Hello World');
-}
-);
+// const server = http.createServer((req, res) =>{
+//   res.statusCode = 200;
+//   res.setHeader('Content-type', 'text/plain');
+//   res.end('Hello World');
+// }
+// );
 
-server.listen(port, hostname, () =>{
-  console.log(`server running at http://${hostname}:${port}/`);
+// server.listen(port, hostname, () =>{
+//   console.log(`server running at http://${hostname}:${port}/`);
 
-}
-);
+// }
+// );
 
 
